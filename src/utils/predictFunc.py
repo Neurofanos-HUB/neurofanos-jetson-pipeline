b95ab4aff6b2060bdf152f272c2569823809020c
Tony Cui <ycui982@aucklanduni.ac.nz>
2023-06-16 21:39:53 +1200

    feat: basic pipeline setup

---
+++ src/utils/predictFunc.py
@@ -0,0 +1,31 @@
+import onnxruntime as rt
+
+from models.item_model import HousingFeatures
+from models.item_model import PredictionResult
+from utils import config
+import numpy
+import pickle
+
+session = rt.InferenceSession(config.MODEL_PATH)
+first_input_name = session.get_inputs()[0].name
+first_output_name = session.get_outputs()[0].name
+
+with open('ml_pipelines/features.pickle','rb') as f:
+    feature = pickle.load(f)
+    print("features:",feature)
+
+
+def predict(data: HousingFeatures) -> PredictionResult:
+   try:
+        # Extract data in correct order
+        data_dict = data.dict()
+        to_predict = [data_dict[feature] for feature in feature]
+        
+        # dict to array
+        
+        to_predict = numpy.array(to_predict).reshape(1,-1)
+        print("array:",to_predict)
+        pred_onx = session.run([], {first_input_name: to_predict.astype(numpy.float32)})[0]
+        return {"prediction":float(pred_onx[0])}
+   except:
+        return {"prediction": "error"}
